047d4c01461c541a30f4251937bd849d2f25af28
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-07-06 18:26:29 +0600

    ok

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -463,7 +463,7 @@ def terabox(url):
     sess = session()
     sess.get(url)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/112.0',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36',
         'Accept': 'application/json, text/plain, */*',
         'Accept-Language': 'en-US,en;q=0.5',
         'Content-Type': 'application/json',
@@ -492,7 +492,7 @@ def terabox(url):
     if res["result"]:
         return res["data"]
     else:
-        return DirectDownloadLinkException('ERROR: Link not found')
+        return 'ERROR: Link not found'
 
 
 def filepress(url):
