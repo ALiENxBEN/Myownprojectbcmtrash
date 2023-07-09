ca0c61e5764c7d6c203a21769e0cacf5b53971c8
yesiamshojib@gmail.com <yesiamshojib@gmail.com>
2023-07-09 22:35:51 +0600

    ff

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -3,6 +3,7 @@ from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from pyrogram.filters import command, regex
 from psutil import cpu_percent, virtual_memory, disk_usage
 from time import time
+from quoters import Quote
 
 from bot import status_reply_dict_lock, download_dict, download_dict_lock, botStartTime, DOWNLOAD_DIR, Interval, config_dict, bot
 from bot.helper.telegram_helper.filters import CustomFilters
